82b4dafb7e3b22d372f5538e1b61072db48795a0
Orlando Loza <orlandolozaarr@gmail.com>
2020-04-20 13:24:42 -0500

    timer function

--- src/app/pages/cubing/cubing.page.ts
+++ src/app/pages/cubing/cubing.page.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cubing.page.scss'],
 })
 export class CubingPage implements OnInit {
-
+ 
   cubesList:string[] = [];
+
+  contentBg = "yellow-bg";
+
+  text1 = "Best: N/A\nAVG 5: N/A\nAVG 12: N/A\nAVG 50: N/A"
+
+  text2 = "Worst: N/A\nAVG 100: N/A\nAVG 1000: N/A\nSolves: N/A"
 
   scramble:Scramble= {
     sequence: [],
@@ -39,6 +45,16 @@ export class CubingPage implements OnInit {
 
   nextScramble(){
     this.ionViewWillEnter();
+  }
+
+  ready(){
+    console.log('ready...');
+    document.body.style.setProperty('--my-var', 'var(--ion-color-success)');
+  }
+
+  stopTimer(){
+    console.log('stop...');
+    document.body.style.setProperty('--my-var', 'var(--ion-color-danger)');
   }
 
 }
