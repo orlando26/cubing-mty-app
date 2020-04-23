a9998bb65bba0220f339e5794e0b91c2c8df4a95
lu0ear <lucero.alvaradorz@gmail.com>
2020-04-23 17:11:30 -0500

    Register page v01, same theme as Login page

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  images: string[] = ['background_sign'];
+  pagesSign: string = this.images[0];
 
+  showPassword = false;
+  passwordToggleIcon = 'eye';
+  togglePassword():void{
+    this.showPassword = !this.showPassword;
+    
+    if(this.passwordToggleIcon == 'eye'){
+      this.passwordToggleIcon = 'eye-off';
+    }else{
+      this.passwordToggleIcon = 'eye';
+    }
+  }
   constructor() { }
 
   ngOnInit() {
