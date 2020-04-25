ecbe3ea6c65e1420807a2956f367f79cd67245fd
lu0 <lucero.alvaradorz@gmail.com>
2020-04-25 15:55:46 -0500

    Getting data from the register page, need to display cities

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { RegisterPage } from '../register/register.page';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -30,14 +30,49 @@ export class LoginPage implements OnInit {
     if(this.nickname && this.password){
       console.log("nickname:" + this.nickname);
       console.log("password:" + this.password);
+      console.log("Navigating to Home (timer)")
       this.router.navigateByUrl('/home');
     }else{
       console.log("Empty fields");
-      alert('Por favor llena todos los campos v:');
+      // alert('Por favor llena todos los campos v:');
+      this.simpleToast();
     }
   }
+
+  async simpleToast(){
+    const toast = await this.toastController.create({
+      message: 'Por favor llena todos los campos v:',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async coolToast() {
+    const toast = await this.toastController.create({
+      message: 'Por favor llena todos los campos',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'un texto v:',
+          handler: () => {
+            console.log('toast icon clicked');
+          }
+        }, {
+          text: 'OK',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
  
-  constructor(private router: Router) {}
+  constructor(private router: Router, public toastController: ToastController) {}
 
   ngOnInit() {
   }
