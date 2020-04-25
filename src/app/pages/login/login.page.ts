18dcf8335d4c999b770337de9eb61c007ffae807
lu0 <lucero.alvaradorz@gmail.com>
2020-04-25 15:59:23 -0500

    ups...

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { RegisterPage } from '../register/register.page';
 import { ToastController } from '@ionic/angular';
 
 @Component({
